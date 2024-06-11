ba1aa954acdcb3ee46be4566e423fb82eabfe7f0
Justin Young <justin_dean_young@yahoo.com>
2024-06-11 17:03:25 -0400

    pwacofnig

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,23 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Mookies Exercise App",
   description: "Quick Utility to log exercise",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["Exercise", "tracking", "workout"],
+  authors: [
+    { name: "Justin Young" },
+    {
+      name: "Justin Young",
+      url: "https://github.com/jyoung4242",
+    },
+  ],
+  themeColor: "#ffffff",
+  colorScheme: "light",
+  viewport: "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "public/exercise-app.png" },
+    { rel: "icon", url: "public/exercise-app.png" },
+  ],
 };
 
 export default function RootLayout({
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
-/** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
   },
 };
 
-export default nextConfig;
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA(nextConfig);
